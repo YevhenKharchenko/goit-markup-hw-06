223cf45596fee1288e05d711afd766e2c161fba5
YevhenKharchenko <evgenvalerych@gmail.com>
2023-12-29 11:05:19 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,22 +2,30 @@ const modalBtn = document.querySelector(".main-btn");
 const modal = document.querySelector(".modal-container");
 const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener("click", () => {
-  modal.classList.add("is-open");
-});
+modalBtn.addEventListener("click", onModalButtonClick);
 
-closeBtn.addEventListener("click", () => {
+function onModalButtonClick() {
+  modal.classList.add("is-open");
+}
+
+closeBtn.addEventListener("click", onCloseButtonClick);
+
+function onCloseButtonClick() {
   modal.classList.remove("is-open");
-});
+}
 
 const contextMenu = document.querySelector(".context-menu");
 const menuBtn = document.querySelector(".open-menu-btn");
 const closeContextBtn = document.querySelector(".context-menu-close-btn");
 
-menuBtn.addEventListener("click", () => {
-  contextMenu.classList.add("is-open");
-});
+menuBtn.addEventListener("click", onMenuButtonClick);
 
-closeContextBtn.addEventListener("click", () => {
+function onMenuButtonClick() {
+  contextMenu.classList.add("is-open");
+}
+
+closeContextBtn.addEventListener("click", onCloseContextButtonClick);
+
+function onCloseContextButtonClick() {
   contextMenu.classList.remove("is-open");
-});
+}
